2424bd7b5fe2da2fd5886dffa289e8773b9c64bf
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2021-10-31 23:17:22 +0200

    init

--- helloworld/src/app/test/test.component.ts
+++ helloworld/src/app/test/test.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  title="hello world";
+  name="Geoupe2 Octobre";
+ imageURL="https://i.pinimg.com/originals/4e/af/a6/4eafa626e4854651ee1afd4a446c782a.jpg";
+ dates=new Date();
+ names=['Ali','Ahmed','Eya','Imen','Asma'];
+ peopleList=[
+   {id:1,firstname:"Ahmed"},
+   {id:2,firstname:"Ali"},
+ ]
+  constructor() { 
+  
+  }
 
   ngOnInit(): void {
   }
