9ee2b09e1f7e62b38f25ff5553c0495ed0ef5ebb
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2021-11-10 21:27:28 +0200

    init

--- helloworld/src/app/test/test.component.ts
+++ helloworld/src/app/test/test.component.ts
@@ -1,12 +1,13 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup,FormControl,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+myForm:FormGroup
 
   title="hello world";
   name="Geoupe2 Octobre";
@@ -17,11 +18,48 @@ export class TestComponent implements OnInit {
    {id:1,firstname:"Ahmed"},
    {id:2,firstname:"Ali"},
  ]
-  constructor() { 
+  constructor(private fb:FormBuilder) { 
+    let formControls={
+      firstname:new FormControl('',[
+        Validators.required,
+        Validators.pattern("[A-Za-z]+"),
+        Validators.minLength(3)
+      ]),
+      lastname:new FormControl('',[
+        Validators.required,
+        Validators.pattern("[A-Za-z]+"),
+        Validators.minLength(3)
+      ]),
+      phone:new FormControl('',[
+        Validators.required,
+        Validators.pattern("[0-9]+")
+      ]),
+      email:new FormControl('',[
+        Validators.required,
+        Validators.email
+      ])
+
+    }
+    this.myForm=this.fb.group(formControls)
   
+  }
+  get firstname(){
+    return this.myForm.get('firstname')
+  }
+  get lastname(){
+    return this.myForm.get('lastname')
+  }
+  get phone(){
+    return this.myForm.get('phone')
+  }
+  get email(){
+    return this.myForm.get('email')
   }
 
   ngOnInit(): void {
+  }
+  save() {
+    console.log(this.myForm.value);
   }
 
 }
